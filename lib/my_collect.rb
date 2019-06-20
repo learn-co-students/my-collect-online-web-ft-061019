97b7bf7bdd88d684d23050c63607f6e7c2da74a8
Adam Ervin <adamjervin@gmail.com>
2019-06-20 23:50:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(language)
+  lang = []
+   i = 0
+  while i < language.length
+    lang << yield(language[i])
+    i = i + 1
+  end
+  lang
+end
+
+
+my_collect(["ruby", "javascript", "python","objective-c"]) {|language| language.upcase}
 
 
